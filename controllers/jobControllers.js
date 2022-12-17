f33cd559ced56a53da24e94d37d524a300fe3e5c
Sahil Kumar <kumarsahil8981@gmail.com>
2022-12-17 14:31:19 +0530

    last

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -2,8 +2,22 @@ const asyncHandler = require("express-async-handler");
 const Jobs = require("../models/jobModel");
 
 const getJobs = asyncHandler(async (req,res) => {
-    const jobs = await Jobs.find();
-    res.json(jobs)
+    let {
+        page = 1,
+        limit = 10,
+        sortBy = "id",
+        order = "asc",
+      } = req.query;
+      try {
+        
+        const list = await Jobs.find().skip((page - 1) * limit).limit(limit).sort({ [sortBy]: order === "asc" ? 1 : -1 });
+       
+       const count = await Jobs.find().count();
+       // console.log(count)
+       res.status(200).send({ data: list, totalPages: Math.ceil(count / limit)});
+     } catch (err) {
+       res.status(500).send(err);
+    }
 })
 
 const createJobs = asyncHandler(async (req,res) => {
